d68ee81bf1b3dcd89d937315d0cb6bccbb7a2023
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-12-10 13:55:09 +0700

    Получить список форм собственности удается, но не получается его отобразить в шаблоне.

--- src/main/java/com/example/TenderBidding/controllers/RegistrationPageController.java
+++ src/main/java/com/example/TenderBidding/controllers/RegistrationPageController.java
@@ -1,13 +1,26 @@
 package com.example.TenderBidding.controllers;
 
+import com.example.TenderBidding.models.OwnershipType;
+import com.example.TenderBidding.repositories.OwnershipTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class RegistrationPageController {
 
+    @Autowired
+    private OwnershipTypeRepository ownershipTypeRepository;
+
     @GetMapping("/registration")
-    public String showRegistrationPage() {
-        return "registrationpage"; // Название HTML-шаблона
+    public String showRegistrationForm(Model model) {
+        List<OwnershipType> ownershipTypes = ownershipTypeRepository.findAll();
+        System.out.println("Ownership Types: " + ownershipTypes); // Для отладки
+        model.addAttribute("ownershipTypes", ownershipTypes);
+        return "registrationpage"; // имя вашего файла шаблона
     }
+
 }
