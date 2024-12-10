fd81be121179a69664765d57336845e4a5baf609
delokoseni <107359196+delokoseni@users.noreply.github.com>
2024-12-10 14:40:32 +0700

    Добавлено оформление для главной страницы сайта.

--- src/main/java/com/example/TenderBidding/controllers/MainPageController.java
+++ src/main/java/com/example/TenderBidding/controllers/MainPageController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
+
     @GetMapping("/")
-    public String mainpage(){
+    public String showMainPage() {
         return "mainpage";
     }
 }
